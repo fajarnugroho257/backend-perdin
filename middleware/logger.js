50d7ce210286c0cf7f13e54de62f9af69f670ebb
fajarnugroho257 <fajarnugroho274@gmail.com>
2025-03-16 22:17:51 +0700

    delete

--- middleware/logger.js
+++
@@ -1,6 +0,0 @@
-const logger = (req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  next(); // Lanjut ke middleware berikutnya atau handler
-};
-
-module.exports = logger;
